184654e2f5c6e073a5f100e6714020cff80fcaec
Technoank <technoank22@gmail.com>
2026-02-13 18:46:28 +0530

    feat - + Config File
    Chore -
    + Bug Fixes UIBasictest File

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -13,7 +13,7 @@ test('Page Playwright test', async ({ page }) => {
 });
 
 
-test.only('Page Check Title',async ({page}) =>
+test('Page Check Title',async ({page}) =>
 {
   const firstName = page.locator("#mat-input-0");
   const lastName =  page.locator("#mat-input-1");
@@ -29,24 +29,5 @@ test.only('Page Check Title',async ({page}) =>
   console.log(await page.title())
   //
   await expect(page).toHaveTitle("ChottuLink | Sign Up");
-
-  // CSS and xpath locators are availbele
-  await Allowall.waitFor({state: 'visible'});
-  await Allowall.click();
-  // in fill method if you add "" it clears the previous value 
-  await firstName.fill("");
-  await lastName.fill("Karl");
-  await email.fill("cryptotokenaxs+32@gmail.com");
-  await password.fill("Technoank@11");
-  await confirmPassword.fill("Technoank@11");
-  await CheckBoxAllow.click();
-  await signIN.click();
-  // TO Extract the text from the field 
-  // Webdriver wait is automatically applied by default by playwright which is in playwright.config
-  console.log(await page.locator('[id="mat-mdc-error-0"]').textContent());
-  // Now to Check Error message if correct or not
-  await expect(page.locator('[id="mat-mdc-error-0"]')).toContainText("First Name is required");
-  await page.pause();
-
 
 });
